ccb069efa6dfd2441278aca4479fd4541af6ec6f
daiana-herrera <daiana.herrera@accenture.com>
2023-01-05 09:25:08 -0300

    sec10!

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -2,7 +2,7 @@ const { response, request } = require('express');
 const bcryptjs = require ('bcryptjs');
 
 const Usuario = require('../models/usuario');
-const usuario = require('../models/usuario');
+
 
 const usuariosGet = async (req = request,res = response )=> {
  // const {q, nombre = 'no Name', apiKey } = req.query;
@@ -66,7 +66,7 @@ const usuariosPatch = (req,res = response )=> {
 }
 const usuariosDelete = async (req,res = response )=> {
 
-    const {id } = req.params;
+    const { id } = req.params;
 
     //Fisicamente lo borro
     //const usuario = await Usuario.findByIdAndDelete( id );
--- models/server.js
+++ models/server.js
@@ -8,7 +8,9 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+
         this.usuariosPath = '/api/usuarios';
+        this.authPath = '/api/auth';
 
         //Conectar a base de datos
         this.conectarDB();
@@ -36,7 +38,11 @@ class Server {
     }
 
     routes() {
-        this.app.use('/api/usuarios', require('../routes/usuarios'));
+
+        this.app.use(this.authPath, require('../routes/auth'));
+        this.app.use(this.usuariosPath, require('../routes/usuarios'));
+
+
     }
 
     listen() {
